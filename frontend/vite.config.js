89ef1604b700593d388e328e0e172779b9726abc
Se Hyeon, Park <qkrtpgus2280@naver.com>
2022-08-09 18:02:22 +0900

    vite 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  if (command === "dev") {
+  if (mode === "development") {
     return {
+      server: {
+        host: "0.0.0.0",
+        port: 8080,
+      },
       plugins: [vue()],
     };
   } else {
